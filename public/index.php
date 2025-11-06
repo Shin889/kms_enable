f00f6ab6bef587777bdc154ea22f00a2642e19f2
Shin889 <shienajoy88@gmail.com>
2025-11-06 22:57:37 +0800

    minor word/catsulogo revision

--- public/index.php
+++ public/index.php
@@ -13,7 +13,7 @@ if ($u) {
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>KMS Enable Recruitment and Onboarding</title>
+    <title>KMS Enable Recruitment</title>
     <link rel="stylesheet" href="assets/utils/index.css" />
 </head>
 
